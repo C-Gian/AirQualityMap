0683fa6ec0842d9bb7f01f946970468be78698f0
C-Gian <gianluca.e.lav@gmail.com>
2023-07-19 21:55:34 +0200

    ??

--- air-quality-map/src/components/Sidebar.jsx
+++ air-quality-map/src/components/Sidebar.jsx
@@ -2,23 +2,82 @@ import React, { useEffect, useRef } from "react";
 import { Chart } from "chart.js/auto";
 
 const Sidebar = ({ infos, onButtonClick }) => {
-  const stateInfo = infos.stato;
-  const hoveredStateColor = infos.colore;
-  //console.log("1", stateInfo);
-  const r = Math.round(hoveredStateColor.r * 255);
-  const g = Math.round(hoveredStateColor.g * 255);
-  const b = Math.round(hoveredStateColor.b * 255);
+  let name = "";
+  let AQI = "";
+  let lastUpdate = "";
+  let values = [];
+  let dataR = null;
+  let countryPolluttans = {
+    CO: {
+      totalValue: 0,
+      times: 0,
+      fixedValue: 0,
+    },
+    NO2: {
+      totalValue: 0,
+      times: 0,
+      fixedValue: 0,
+    },
+    OZONE: {
+      totalValue: 0,
+      times: 0,
+      fixedValue: 0,
+    },
+    "PM2.5": {
+      totalValue: 0,
+      times: 0,
+      fixedValue: 0,
+    },
+    PM10: {
+      totalValue: 0,
+      times: 0,
+      fixedValue: 0,
+    },
+    SO2: {
+      totalValue: 0,
+      times: 0,
+      fixedValue: 0,
+    },
+  };
+  if (Object.keys(infos.stato)[0] == "USA") {
+    name = "USA";
+    dataR = infos.stato["USA"];
+    AQI = dataR.features[0].properties.countryAQI;
+    lastUpdate = dataR.features[0].lastUpdatedMe;
+    dataR.features.forEach((feature) => {
+      Object.keys(feature.properties.measurements).forEach((poll) => {
+        if (feature.properties.measurements[poll].fixedValue != null) {
+          countryPolluttans[poll].totalValue +=
+            feature.properties.measurements[poll].fixedValue;
+          countryPolluttans[poll].times += 1;
+        }
+      });
+    });
+    Object.keys(countryPolluttans).forEach((key) => {
+      countryPolluttans[key].fixedValue =
+        countryPolluttans[key].totalValue / countryPolluttans[key].times;
+      values.push(countryPolluttans[key].fixedValue);
+    });
+  } else {
+    name = infos.stato.properties.name;
+    AQI = infos.stato.properties.AQI;
+    lastUpdate = infos.stato.lastUpdatedMe;
+    Object.keys(infos.stato.properties.measurements).forEach((key) => {
+      values.push(infos.stato.properties.measurements[key].fixedValue);
+    });
+  }
+  const colorToColor = infos.colore;
+  const r = Math.round(colorToColor.r * 255);
+  const g = Math.round(colorToColor.g * 255);
+  const b = Math.round(colorToColor.b * 255);
   const hexColor = `#${r.toString(16).padStart(2, "0")}${g
     .toString(16)
     .padStart(2, "0")}${b.toString(16).padStart(2, "0")}`;
-  let values = [];
-  Object.keys(stateInfo.properties.measurements).forEach((key) => {
-    values.push(stateInfo.properties.measurements[key].fixedValue);
-  });
+
   const chartRef = useRef(null);
   // Dati per il grafico
   const data = {
-    labels: Object.keys(stateInfo.properties.measurements),
+    labels: Object.keys(countryPolluttans),
     datasets: [
       {
         label: "Polluttans levels",
@@ -94,12 +153,12 @@ const Sidebar = ({ infos, onButtonClick }) => {
   }, [data, options]);
 
   return (
-    <div className="w-400 h-800 p-8 bg-gray-600 z-30 fixed">
+    <div className="w-400 h-full p-8 bg-gray-600 z-30 fixed">
       <button className="close-button" onClick={onButtonClick}>
         &#10005;
       </button>
       <div className="flex items-center">
-        <span className="text-4xl text-white">{stateInfo.properties.name}</span>
+        <span className="text-4xl text-white">{name}</span>
       </div>
       <div className="flex flex-col w-fit h-fit items-center  justify-between ">
         <div className="flex w-full h-fit items-center mt-4 overflow-hidden">
@@ -110,12 +169,12 @@ const Sidebar = ({ infos, onButtonClick }) => {
             className=" rounded-2xl p-3"
             style={{ backgroundColor: hexColor }}
           >
-            <h2 className="text-white text-xl">{stateInfo.properties.AQI}</h2>
+            <h2 className="text-white text-xl">{AQI}</h2>
           </div>
         </div>
         <div className="flex w-full h-fit justify-between items-center mt-3">
           <h2 className="text-white text-xl items-center mr-5">Last Update:</h2>
-          <span className="text-l text-white">{stateInfo.lastUpdatedMe}</span>
+          <span className="text-l text-white">{lastUpdate}</span>
         </div>
       </div>
       <div>
